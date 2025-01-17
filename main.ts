454195882a272a3b3c8d39ed14b806e19051735e
azure-553 <simmijin816@gmail.com>
2025-01-18 02:15:46 +0900

    feat: fetch Notion API

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-console.log("Hello world");
+import { Client } from "@notionhq/client";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+async function fetchNotionData() {
+  try {
+    if (!databaseId) {
+      throw new Error("Missing NOTION_DATABASE_ID environment variable");
+    }
+
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    console.log("Notion Database Data:", response.results);
+  } catch (error) {
+    console.error("Error fetching Notion data:", error.message);
+  }
+}
+
+fetchNotionData();
